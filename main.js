886cdc6c969aa408ad0dab7421072a520121e2d7
ana-chiqueira <114490411+ana-chiqueira@users.noreply.github.com>
2023-09-11 11:41:58 -0300

    11/09

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const controle = document.querySelector("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+console.log(estatisticas)
 const pecas ={
     "bracos":{
         "forca": 29,
@@ -8,12 +10,34 @@ const pecas ={
     },
     "blindagem":{
         "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
     
 }
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
         manipulaDados(evento.target.textContent, evento.target.parentNode)
+        atualizaEstatisticas(evento.taeget.dataset.peca)
     })
 })
 
@@ -25,4 +49,12 @@ function manipulaDados(operacao, controle) {
     } else {
         peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatisticas(peca){
+    console.log(peca[peca])
+
+    estatisticas.forEach( (elememto) =>{
+        console.log(elemento.textContent)
+    })
 }
